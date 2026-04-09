b471db6616eca29b39d7d8205253c710f448823d
Enzzo Ferreira <enzzoferreira69@gmail.com>
2026-04-09 16:44:31 -0300

    Add files via upload

---
+++ Fila Parada/Main.java
@@ -0,0 +1,34 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    try
+	    {
+    	    Fila<String> fil = new Fila<String> (1000000);
+	    
+			long inicio = System.currentTimeMillis();			
+			for (int i=0; i<1000000; i++)
+    	        fil.guardeUmItem("COTUCA");
+            long fim = System.currentTimeMillis();			
+    	    long tempoDecorrido = fim-inicio;
+    	    System.out.println("Para encher a fila levou " +
+    	                       tempoDecorrido + " ms");
+    	    
+            inicio = System.currentTimeMillis();			
+			for (int i=0; i<1000000; i++)
+			{
+				String desenfileirado = fil.recupereUmItem();
+				// System.out.println(desenfileirado);
+    	        fil.removaUmItem();
+    	    }
+            fim = System.currentTimeMillis();			
+    	    tempoDecorrido = fim-inicio;
+    	    System.out.println("Para esvaziar a fila levou " +
+    	                       tempoDecorrido + " ms");
+	    }
+	    catch (Exception erro)
+	    {
+	        System.err.println (erro.getMessage());
+	    }
+	}
+}
